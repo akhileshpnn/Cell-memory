72f907ee49078c47e7d00d617edda40a0869072c
akhileshpnn <41164857+akhileshpnn@users.noreply.github.com>
2022-01-20 16:37:03 +0100

    Update pressure_functions.py

--- Physical model of single-cell chemotaxis/pressure_functions.py
+++ Physical model of single-cell chemotaxis/pressure_functions.py
@@ -11,7 +11,6 @@ def pressure_pro(egfrp):
 def pressure_ret(egfrp):
     
     kret=0.05 #nN/um2
-    # kret=0.11 #nN/um2
 
     pret=-kret*(np.mean(egfrp)-egfrp)/(np.mean(egfrp)-np.max(egfrp))
     pret[np.argwhere(pret<0)]=0
@@ -22,22 +21,17 @@ def pressure_ret(egfrp):
 
 def pressure_area(A,A0,mem_len,egfrp):
 
-    # karea = 0.01
     karea = 0.02
     
     parea=karea*(A0-A)
     return parea*np.ones(mem_len)
 
 def pressure_tension(cur,rc):
-#    kten=0.1 #nN/um
-    # rc=5.1 #um  
-    
-    # kten=0.011 #nN/um
+
     kten=0.1
-    # rc=5.1 #um 
+
     cur = np.round(cur,2)
     pten=kten*(cur-1/rc)    
-    # pten=np.round(pten,2)
     
     return pten
 
